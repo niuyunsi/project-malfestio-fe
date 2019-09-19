5b68f1a6894718c402d4dbb02b762223f4365dde
Yunsi Niu <niuyunsi@gmail.com>
2019-09-19 21:11:31 +0800

    add config

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { endpoint, prodEndpoint } from '../config';
 
-export const Demo = () => (
-  <div>demo</div>
-);
+export const Demo = () => {
+  const url = process.env.NODE_ENV === 'production' ? prodEndpoint : endpoint;
+
+  const handleFetchAnimals = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <div>demo</div>
+      <button onClick={handleFetchAnimals}>fetch animals</button>
+    </div>
+  );
+};
